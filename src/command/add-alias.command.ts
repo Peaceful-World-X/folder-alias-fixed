8e9660b8c1dafff98408fe0e05782d8b1479c4fc
Joker <peaceful_world@qq.com>
2025-08-24 23:13:08 +0800

    init~

--- src/command/add-alias.command.ts
+++ src/command/add-alias.command.ts
@@ -3,7 +3,7 @@ import { useCommand } from "reactive-vscode";
 import * as vscode from "vscode";
 
 function addAlias(workspace: vscode.WorkspaceFolder, fileAlias: UseFileAliasReturn) {
-  const { publicConfig, configFile, resetConfig, savePublic, changeEmitter } = fileAlias;
+  const { publicConfig, privateConfig, configFile, resetConfig, savePrivate, savePublic, changeEmitter } = fileAlias;
 
   useCommand("folder-alias.addAlias", (uri: vscode.Uri) => {
     const relativelyPath = uri.path.substring(workspace.uri.path.length + 1);
@@ -13,15 +13,32 @@ function addAlias(workspace: vscode.WorkspaceFolder, fileAlias: UseFileAliasRetu
         ? configFile.value[relativelyPath].description
         : "folder-alias",
     };
-    vscode.window.showInputBox(inputConfig).then((alias) => {
-      resetConfig();
-      if (alias) {
-        publicConfig.value[relativelyPath] = {
-          ...publicConfig.value[relativelyPath],
-          description: alias,
-        };
-        savePublic();
-        changeEmitter(uri);
+    vscode.window.showQuickPick(["public", "private"]).then((scope) => {
+      if (scope === "private") {
+        vscode.window.showInputBox(inputConfig).then((alias) => {
+          resetConfig();
+          if (alias) {
+            privateConfig.value[relativelyPath] = {
+              ...privateConfig.value[relativelyPath],
+              description: alias,
+            };
+            savePrivate();
+            changeEmitter(uri);
+          }
+        });
+      }
+      else {
+        vscode.window.showInputBox(inputConfig).then((alias) => {
+          resetConfig();
+          if (alias) {
+            publicConfig.value[relativelyPath] = {
+              ...publicConfig.value[relativelyPath],
+              description: alias,
+            };
+            savePublic();
+            changeEmitter(uri);
+          }
+        });
       }
     });
   });
